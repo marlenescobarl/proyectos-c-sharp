4334608b4979a76995bd0de03d0ba0e77c356da9
marlenescobarl <marlenescobarlima11@gmail.com>
2021-02-23 19:14:32 -0600

    Se agregaron los archivos y se inicio Hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HELLO_WORLD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HELLO WORLD IN C#!");
+        }
+    }
+}
